7b47887d236ce9dbcf191b147e841311987abe0e
Gali998 <galimannage6.gm@gmail.com>
2022-05-29 10:25:45 +0530

    login page added

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,4 +1,22 @@
 package com.qa.pages;
 
-public class LoginPage {
+import com.qa.base.BasePage;
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+
+    private static final By USERNAME = By.id("user-name");
+
+    private static final By PASSWORD = By.id("password");
+
+    private static final By LOGIN_BTN = By.id("login-button");
+
+    public void enterCredentials(String username,String password){
+        typeText(USERNAME,username);
+        typeText(PASSWORD,password);
+    }
+
+    public void clickLogin(){
+        clickElement(LOGIN_BTN);
+    }
 }
