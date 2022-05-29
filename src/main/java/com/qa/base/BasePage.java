877bf33cfcd50e826d332aa6bb217d2607fecfbd
Gali998 <galimannage6.gm@gmail.com>
2022-05-29 09:53:13 +0530

    Apply POM structure and base page added

---
+++ src/main/java/com/qa/base/BasePage.java
@@ -0,0 +1,55 @@
+package com.qa.base;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    private static WebDriver driver;
+
+    private static int timeout = 10;
+
+    public void initWebDriver(){
+        ChromeDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    public void quitWebDriver(){
+        if(null!=driver){
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public void clickElement(By locator){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,timeout);
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            element.click();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void typeText(By locator,String text){
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public boolean isVisible(By locator){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,timeout);
+            return wait.until(ExpectedConditions.visibilityOf(driver.findElement(locator)))!=null;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+}
