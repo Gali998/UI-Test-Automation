d7468c68d7ea6007149f47b9513b88a9a2dcf3bb
Gali998 <galimannage6.gm@gmail.com>
2022-05-29 10:53:01 +0530

    inventory page added

--- src/main/java/com/qa/pages/InventoryPage.java
+++ src/main/java/com/qa/pages/InventoryPage.java
@@ -1,4 +1,28 @@
 package com.qa.pages;
 
-public class InventoryPage {
+import com.qa.base.BasePage;
+import org.openqa.selenium.By;
+
+public class InventoryPage extends BasePage {
+
+    private static final By INVENTORY = By.id("inventory_container");
+
+    private static final By PRODUCT = By.xpath("//a[@id='item_4_title_link']/div");
+
+    private static final By ADD_CART_BTN = By.id("add-to-cart-sauce-labs-backpack");
+
+    public boolean isInventoryVisible(){
+        return isVisible(INVENTORY);
+    }
+
+    public void clickProduct(){
+        clickElement(PRODUCT);
+    }
+
+    public void clickAddToCart(){
+        clickElement(ADD_CART_BTN);
+    }
+
+
+
 }
